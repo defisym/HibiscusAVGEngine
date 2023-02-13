ba11ca79ec2f77c3e8d9ba587c122fdb4120cdec
Defisym <55148235+defisym@users.noreply.github.com>
2023-02-13 23:01:03 +0800

    Update ContentGenerator.bat

--- Utilities/ContentGenerator/ContentGenerator.bat
+++ Utilities/ContentGenerator/ContentGenerator.bat
@@ -9,6 +9,11 @@ Set SteamCMDPath=F:\DEV\_Steamworks_SDK\tools\ContentBuilder\builder\steamcmd.ex
 
 Set curPath=%~dp0
 
+Set Encrypter_CLI=F:\DEV\HibiscusAVGEngine\Utilities\Encrypter_CLI\Encrypter_CLI.exe
+Set Encrypter_Key=12345678ABCDEFGH
+
+Set Settings_CLI=F:\DEV\HibiscusAVGEngine\Utilities\Settings_CLI\Settings_CLI.exe
+
 @echo delete old content...
 
 rem Clean Content Path
@@ -21,30 +26,37 @@ rmdir /s /q "%ContentPath%\savings\"
 rmdir /s /q "%ContentPath%\settings\"
 rmdir /s /q "%ContentPath%\localization\"
 rmdir /s /q "%ContentPath%\Modules\"
+rmdir /s /q "%ContentPath%\%AppName%_Text\"
 
 @echo Generate New Content...
 
 rem Update Content
 xcopy "%curPath%\data\*.*" "%ContentPath%\data\" /s /e
-echo f | xcopy "%curPath%\settings\settings_Template.ini" "%ContentPath%\settings\setting.ini" /s /e
-
 xcopy "%curPath%\savings\_Sys\*.*" "%ContentPath%\savings\_Sys\" /s /e
+echo f | xcopy "%curPath%\settings\settings_Template.ini" "%ContentPath%\settings\settings.ini" /s /e
+echo f | xcopy "%curPath%\settings\settings_Template.ini" "%ContentPath%\settings\settings_Template.ini" /s /e
 echo f | xcopy "%curPath%\savings\_Global\_GlobalProgress_Template" "%ContentPath%\savings\_Global\_GlobalProgress" /s /e
 echo f | xcopy "%curPath%\savings\_Global\Appreciation_Definition_Template" "%ContentPath%\savings\_Global\Appreciation_Definition" /s /e
 echo f | xcopy "%curPath%\savings\_Global\Appreciation_Progress_Template" "%ContentPath%\savings\_Global\Appreciation_Progress" /s /e
-echo f | xcopy "%curPath%\savings\_Global\Data_Template" "%ContentPath%\savings\qdata.sav" /s /e
-
+echo f | xcopy "%curPath%\savings\_Global\Data_Template" "%ContentPath%\savings\_Global\Data_Template" /s /e
 xcopy "%curPath%\Modules\*.*" "%ContentPath%\Modules\" /s /e
 xcopy "%curPath%\localization\*.*" "%ContentPath%\localization\" /s /e
 
 echo f | xcopy "%curPath%\%AppName%.dat" "%ContentPath%" /s /e
+echo f | xcopy "%curPath%\%AppName%.exe" "%ContentPath%\%AppName%.exe" /s /e
 
-@REM echo f | xcopy "%curPath%\%AppName%.exe" "%ContentPath%\%AppName%.exe" /s /e
 @REM echo f | xcopy "%curPath%\%AppName%_Wrapper.exe" "%ContentPath%\%AppName%.exe" /s /e
 
-REM Delete Temp Files
-del "%ContentPath%\CrashLog.Log" /f /s /q
+@REM Delete Temp Files
+del "%ContentPath%\.gitignore" /f /s /q
+
+del "%ContentPath%\NOENCRYPT" /f /s /q
+del "%ContentPath%\_HWDECODE" /f /s /q
+
+del "%ContentPath%\Application resizing errors.txt" /f /s /q
+
 del "%ContentPath%\ContentGenerator.bat" /f /s /q
+del "%ContentPath%\Publish.bat" /f /s /q
 
 for /r "%ContentPath%" %%F in (*.file) do (
 	del "%%F" /f /s /q	
@@ -58,12 +70,13 @@ for /r "%ContentPath%" %%F in (*.001) do (
 	del "%%F" /f /s /q	
 )
 
-for /r "%ContentPath%\_Docs" %%F in (*.*) do (
+for /r "%ContentPath%\data\Assets\__Movies" %%F in (*.*) do (
 	del "%%F" /f /s /q	
 )
 
-rmdir /s /q "%ContentPath%\_Docs\"
+rmdir /s /q "%ContentPath%\data\Assets\__Movies\"
 
+@REM Dialogue
 for /r "%ContentPath%\data\dialogue\_External" %%F in (*.*) do (
 	del "%%F" /f /s /q	
 )
@@ -82,20 +95,85 @@ for /r "%ContentPath%\data\dialogue\_Template" %%F in (*.*) do (
 
 rmdir /s /q "%ContentPath%\data\dialogue\_Template\"
 
-for /r "%ContentPath%\data\dialogue\labels" %%F in (*.*) do (
+for /r "%ContentPath%\data\dialogue\__Merged" %%F in (*.*) do (
 	del "%%F" /f /s /q	
 )
 
-rem Steam Init
+rmdir /s /q "%ContentPath%\data\dialogue\__Merged\"
+
+for /r "%ContentPath%\data\dialogue\__Old" %%F in (*.*) do (
+	del "%%F" /f /s /q	
+)
+
+rmdir /s /q "%ContentPath%\data\dialogue\__Old\"
+
+for /r "%ContentPath%\data\dialogue\.vscode" %%F in (*.*) do (
+	del "%%F" /f /s /q	
+)
+
+rmdir /s /q "%ContentPath%\data\dialogue\.vscode\"
+
+del "%ContentPath%\data\dialogue\_Debug_Order.asc" /f /s /q
+del "%ContentPath%\data\dialogue\_initial.asc" /f /s /q
+del "%ContentPath%\data\dialogue\_Ref.asc" /f /s /q
+
+@REM Text
+for /r "%ContentPath%\Mobius_Text" %%F in (*.*) do (
+	del "%%F" /f /s /q	
+)
+
+rmdir /s /q "%ContentPath%\Mobius_Text\"
+
+@echo Encrypt Files...
+for /r "%ContentPath%\data\Assets" %%F in (*.*) do (
+	%Encrypter_CLI% -f %%F --encrypt --key %Encrypter_Key%
+)
+
+for /r "%ContentPath%\data\dialogue" %%F in (*.*) do (
+	%Encrypter_CLI% -f %%F --encrypt --key %Encrypter_Key%
+)
+
+for /r "%ContentPath%\data\Graphics" %%F in (*.*) do (
+	%Encrypter_CLI% -f %%F --encrypt --key %Encrypter_Key%
+)
+
+for /r "%ContentPath%\savings\_Global" %%F in (*.*) do (
+	%Encrypter_CLI% -f %%F --encrypt --key %Encrypter_Key%
+)
+
+@echo Steam Init
 Set SteamUser=defisym
 REM Set SteamPassword=your Password
 Set /p SteamPassword=Please input SteamPassword : 
 Set /p SteamGuard=Please input SteamGuard : 
 
-@echo Building New Content...
-
 rem Build Content
 @REM %SteamCMDPath% +login %SteamUser% %SteamPassword% %SteamGuard% +run_app_build %ScriptPath% +quit
-%SteamCMDPath% +login %SteamUser% %SteamPassword% %SteamGuard% +drm_wrap %APPID% "%curPath%\%AppName%.exe" "%ContentPath%\%AppName%.exe" drmtoolp 0 +run_app_build %ScriptPath% +quit
+@REM %SteamCMDPath% +login %SteamUser% %SteamPassword% %SteamGuard% +drm_wrap %APPID% "%curPath%\%AppName%.exe" "%ContentPath%\%AppName%.exe" drmtoolp 0 +run_app_build %ScriptPath% +quit
+
+@echo Login...
+%SteamCMDPath% +login %SteamUser% %SteamPassword% %SteamGuard% 
+
+@echo DRM...
+%SteamCMDPath% +drm_wrap %APPID% "%curPath%\%AppName%.exe" "%ContentPath%\%AppName%.exe" drmtoolp 0
+
+@echo UpdateHash
+
+for /f "usebackq" %%a in (`%Encrypter_CLI% -f "F:\DEV\Mobius\SteamWorks\_Content\MOBIUS BAND\Mobius.exe" --hash`) do (
+	Set Hash=%%a
+)
+
+@REM @echo %Hash%
+
+%Settings_CLI% -f "%ContentPath%\settings\settings.ini" --unicode --section System --item ExeHash --value %Hash%
+%Settings_CLI% -f "%ContentPath%\settings\settings_Template.ini" --unicode --section System --item ExeHash --value %Hash%
+
+%Encrypter_CLI% -f "%ContentPath%\settings\settings.ini" --encrypt --key %Encrypter_Key%
+%Encrypter_CLI% -f "%ContentPath%\settings\settings_Template.ini" --encrypt --key %Encrypter_Key%
+
+@REM @Pause
+
+@echo Building New Content...
+%SteamCMDPath% +run_app_build %ScriptPath% +quit
 
 exit
