920b913701ea8bfd9d0787dc6aa94fdf9842c000
Defisym <55148235+defisym@users.noreply.github.com>
2023-04-07 11:24:21 +0800

    Create upload.py

---
+++ Utilities/DubSplitter/upload.py
@@ -0,0 +1,38 @@
+import os.path
+import shutil
+import subprocess
+
+from dubSplitter.functions.path import script_path
+
+# requires
+# pip install wheel
+# pip install twine
+
+dist = script_path(__file__) + '\\dist'
+
+print('dist path {}'.format(dist))
+
+print('remove old...')
+if os.path.exists(dist):
+    shutil.rmtree(dist)
+
+print('build new...')
+status = subprocess.call('python setup.py sdist')
+if status != 0:
+    print('build failed')
+    exit(1)
+
+status = subprocess.call('python setup.py bdist_wheel')
+if status != 0:
+    print('build wheel failed')
+    exit(1)
+
+print('uploading...')
+status = subprocess.call('twine upload dist/*')
+
+if status != 0:
+    print('upload failed')
+    exit(1)
+else:
+    print('upload complete')
+    exit(0)
