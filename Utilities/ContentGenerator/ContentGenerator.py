94f200ea6eb214e5c253ecff96393bb307b227ac
Defisym <defisym@outlook.com>
2023-10-05 23:35:15 +0800

    Update ContentGenerator.py

--- Utilities/ContentGenerator/ContentGenerator.py
+++ Utilities/ContentGenerator/ContentGenerator.py
@@ -38,6 +38,16 @@ ScriptPath = curPath + r"\..\SteamWorks\_Script\app_build_2335270.vdf"
 parser = argparse.ArgumentParser(description='Upload to steam.')
 
 
+def iterate(path, callback):
+    for root, ds, fs in os.walk(path):
+        for f in fs:
+            fullname = os.path.join(root, f)
+            filename = os.path.split(fullname)[1]
+            ext = os.path.splitext(filename)[1]
+
+            callback(fullname, filename, ext)
+
+
 def remove_tree(path):
     if os.path.exists(path):
         shutil.rmtree(path)
@@ -53,95 +63,141 @@ def copy_tree(src, dst):
         shutil.copytree(src, dst)
 
 
-def copy_file(path, dst):
-    if os.path.exists(path):
-        shutil.copyfile(path, dst)
+def copy_file(src, dst):
+    if os.path.exists(src):
+        dstPath = os.path.split(dst)[0]
+        # if len(os.path.splitext(dstPath)[1]) == 0:
+        #     dstPath = dst[:dstPath.rfind("\\")]
+
+        if not os.path.exists(dstPath):
+            os.mkdir(dstPath)
+
+        shutil.copyfile(src, dst)
 
 
-def copy(path, dst):
-    if os.path.exists(path):
-        shutil.copy(path, dst)
+def copy(src, dst):
+    if os.path.exists(src):
+        dstPath = os.path.split(dst)[0]
+        # if len(os.path.splitext(dstPath)[1]) == 0:
+        #     dstPath = dst[:dstPath.rfind("\\")]
 
+        if not os.path.exists(dstPath):
+            os.mkdir(dstPath)
+
+        shutil.copy(src, dst)
+
+
+#
+#
+# print(Fore.LIGHTCYAN_EX + '====================')
+# print(Fore.LIGHTCYAN_EX + 'Remove old content...')
+# print(Fore.LIGHTCYAN_EX + '====================')
+#
+# remove_tree(ContentPath)
+#
+# print(Fore.LIGHTCYAN_EX + '====================')
+# print(Fore.LIGHTCYAN_EX + 'Generate new content')
+# print(Fore.LIGHTCYAN_EX + '====================')
+#
+# copy_tree(curPath + r"\data", ContentPath + r"\data")
+# copy_tree(curPath + r"\savings\_Sys", ContentPath + r"\savings\_Sys")
+#
+# copy_file(curPath + r"\savings\_Global\_Cache_Template", ContentPath + r"\savings\_Global\_Cache")
+# copy_file(curPath + r"\savings\_Global\_GlobalProgress_Template",
+#           ContentPath + r"\savings\_Global\_GlobalProgress")
+# copy_file(curPath + r"\savings\_Global\Appreciation_Definition_Template",
+#           ContentPath + r"\savings\_Global\Appreciation_Definition")
+# copy_file(curPath + r"\savings\_Global\Appreciation_Progress_Template",
+#           ContentPath + r"\savings\_Global\Appreciation_Progress")
+# copy_file(curPath + r"\savings\_Global\Data_Template.sav",
+#           ContentPath + r"\savings\_Global\Data_Template.sav")
+#
+# copy_file(curPath + r"\settings\settings_Template.ini", ContentPath + r"\settings\settings.ini")
+# copy_file(curPath + r"\settings\settings_Template.ini", ContentPath + r"\settings\settings_Template.ini")
+# copy_file(curPath + r"\settings\settings_Dynamic.ini", ContentPath + r"\settings\settings_Dynamic.ini")
+#
+# copy_tree(curPath + r"\Modules", ContentPath + r"\Modules")
+# copy_tree(curPath + r"\localization", ContentPath + r"\localization")
+#
+# copy("{}\\{}.dat".format(curPath, AppName), ContentPath)
+# copy("{}\\{}.exe".format(curPath, AppName), ContentPath)
+#
+# print(Fore.LIGHTCYAN_EX + '====================')
+# print(Fore.LIGHTCYAN_EX + 'Remove temp files')
+# print(Fore.LIGHTCYAN_EX + '====================')
+#
+# remove_file(ContentPath + r"\.gitignore")
+#
+# remove_file(ContentPath + r"\NOENCRYPT")
+# remove_file(ContentPath + r"\_HWDECODE")
+#
+# remove_file(ContentPath + r"\Application resizing errors.txt")
+#
+# remove_file(ContentPath + r"\steam_appid.txt")
+#
+# remove_file(ContentPath + r"\ContentGenerator.bat")
+# remove_file(ContentPath + r"\ContentGenerator.py")
+#
+# remove_file(ContentPath + r"\Publish.bat")
+# remove_file(ContentPath + r"\Publish.py")
+#
+#
+# def remover(fullname, filename, ext):
+#     if filename.lower() == "ReadMe.md".lower():
+#         remove_file(fullname)
+#
+#     if ext.lower() == ".file".lower():
+#         remove_file(fullname)
+#     if ext.lower() == ".mfa".lower():
+#         remove_file(fullname)
+#     if ext.lower() == ".001".lower():
+#         remove_file(fullname)
+#
+#
+# iterate(ContentPath, remover)
+#
+# remove_tree(ContentPath + r"\data\_MFAs")
+# remove_tree(ContentPath + r"\data\Assets\__Movies")
+# remove_tree(ContentPath + r"\data\dialogue\_External")
+# remove_tree(ContentPath + r"\data\dialogue\_Test")
+# remove_tree(ContentPath + r"\data\dialogue\_Template")
+# remove_tree(ContentPath + r"\data\dialogue\__Merged")
+# remove_tree(ContentPath + r"\data\dialogue\__Old")
+# remove_tree(ContentPath + r"\data\dialogue\.vscode")
+#
+# remove_tree("{}\\{}_Text".format(curPath, AppName))
+#
+# print(Fore.LIGHTCYAN_EX + '====================')
+# print(Fore.LIGHTCYAN_EX + 'Encrypt files')
+# print(Fore.LIGHTCYAN_EX + '====================')
+#
+#
+# def encrypt_file(file):
+#     if os.path.exists(file):
+#         status = subprocess.call("{} -f \"{}\" --encrypt --key {}".format(Encrypter_CLI, file, Encrypter_Key))
+#
+#         # success
+#         return status == 0
+#     else:
+#         return false
+#
+#
+# encrypt_file(ContentPath + r"\settings\settings_Dynamic.ini")
+#
+# iterate(ContentPath + r"\data\Assets", lambda fullname, filename, ext: encrypt_file(fullname))
+# iterate(ContentPath + r"\data\audio", lambda fullname, filename, ext: encrypt_file(fullname))
+# iterate(ContentPath + r"\data\dialogue", lambda fullname, filename, ext: encrypt_file(fullname))
+# iterate(ContentPath + r"\data\Graphics", lambda fullname, filename, ext: encrypt_file(fullname))
+#
+# iterate(ContentPath + r"\savings\_Global", lambda fullname, filename, ext: encrypt_file(fullname))
+# iterate(ContentPath + r"\savings\_Sys", lambda fullname, filename, ext: encrypt_file(fullname))
+# iterate(ContentPath + r"\localization", lambda fullname, filename, ext: encrypt_file(fullname))
 
 print(Fore.LIGHTCYAN_EX + '====================')
-print(Fore.LIGHTCYAN_EX + 'Remove old content...')
+print(Fore.LIGHTCYAN_EX + 'Steam')
 print(Fore.LIGHTCYAN_EX + '====================')
 
-remove_tree(ContentPath)
+# subprocess.call(SteamCMDPath)
+steam = subprocess.Popen(SteamCMDPath)
+steam.stdin.write("login defisym".encode('utf8'))
 
-print(Fore.LIGHTCYAN_EX + '====================')
-print(Fore.LIGHTCYAN_EX + 'Generate new content')
-print(Fore.LIGHTCYAN_EX + '====================')
-
-copy_tree(curPath + r"\data", ContentPath + r"\data")
-copy_tree(curPath + r"\savings\_Sys", ContentPath + r"\savings\_Sys")
-
-copy_file(curPath + r"\savings\_Global\_Cache_Template", ContentPath + r"\savings\_Global\_Cache")
-copy_file(curPath + r"\savings\_Global\_GlobalProgress_Template",
-          ContentPath + r"\savings\_Global\_GlobalProgress")
-copy_file(curPath + r"\savings\_Global\Appreciation_Definition_Template",
-          ContentPath + r"\savings\_Global\Appreciation_Definition")
-copy_file(curPath + r"\savings\savings\_Global\Appreciation_Progress_Template",
-          ContentPath + r"\_Global\Appreciation_Progress")
-copy_file(curPath + r"\savings\savings\_Global\Data_Template.sav",
-          ContentPath + r"\savings\_Global\Data_Template.sav")
-
-copy_file(curPath + r"\settings\settings_Template.ini", ContentPath + r"\settings\settings.ini")
-copy_file(curPath + r"\settings\settings_Template.ini", ContentPath + r"\settings\settings_Template.ini")
-copy_file(curPath + r"\settings\settings_Dynamic.ini", ContentPath + r"\settings\settings_Dynamic.ini")
-
-copy_tree(curPath + r"\Modules", ContentPath + r"\Modules")
-copy_tree(curPath + r"\localization", ContentPath + r"\localization")
-
-copy("{}\\{}.dat".format(curPath, AppName), ContentPath)
-copy("{}\\{}.exe".format(curPath, AppName), ContentPath)
-
-print(Fore.LIGHTCYAN_EX + '====================')
-print(Fore.LIGHTCYAN_EX + 'Remove temp files')
-print(Fore.LIGHTCYAN_EX + '====================')
-
-remove_file(ContentPath + r"\.gitignore")
-
-remove_file(ContentPath + r"\NOENCRYPT")
-remove_file(ContentPath + r"\_HWDECODE")
-
-remove_file(ContentPath + r"\Application resizing errors.txt")
-
-remove_file(ContentPath + r"\steam_appid.txt")
-
-remove_file(ContentPath + r"\ContentGenerator.bat")
-remove_file(ContentPath + r"\ContentGenerator.py")
-
-remove_file(ContentPath + r"\Publish.bat")
-remove_file(ContentPath + r"\Publish.py")
-
-for root, ds, fs in os.walk(ContentPath):
-    for f in fs:
-        fullname = os.path.join(root, f)
-        filename = os.path.split(fullname)[1]
-        ext = os.path.splitext(filename)[1]
-
-        if filename.lower() == "ReadMe.md".lower():
-            remove_file(fullname)
-
-        if ext.lower() == "file".lower():
-            remove_file(fullname)
-        if ext.lower() == "mfa".lower():
-            remove_file(fullname)
-        if ext.lower() == "001".lower():
-            remove_file(fullname)
-
-remove_tree(ContentPath + r"\data\_MFAs")
-remove_tree(ContentPath + r"\data\Assets\__Movies")
-remove_tree(ContentPath + r"\data\dialogue\_External")
-remove_tree(ContentPath + r"\data\dialogue\_Test")
-remove_tree(ContentPath + r"\data\dialogue\_Template")
-remove_tree(ContentPath + r"\data\dialogue\__Merged")
-remove_tree(ContentPath + r"\data\dialogue\__Old")
-remove_tree(ContentPath + r"\data\dialogue\.vscode")
-
-remove_tree("{}\\{}_Text".format(curPath, AppName))
-
-print(Fore.LIGHTCYAN_EX + '====================')
-print(Fore.LIGHTCYAN_EX + 'Encrypt files')
-print(Fore.LIGHTCYAN_EX + '====================')
